09e41a106cfb561676515a6b143a97e73b5de76b
汪忠祥 <1186355422@qq.com>
2020-05-14 10:29:39 +0800

    init

---
+++ src/main/java/com/java4all/scalog/ScalogApplication.java
@@ -0,0 +1,13 @@
+package com.java4all.scalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScalogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScalogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/java4all/scalog/ScalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java4all.scalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScalogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
