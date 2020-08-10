7a31f9dc588c0942538573cac241083684653fff
党诗林 <dangshilin@touchealth.com>
2020-08-10 11:07:27 +0800

    impl mongodb

--- src/main/java/com/java4all/scalog/configuration/Constants.java
+++ src/main/java/com/java4all/scalog/configuration/Constants.java
@@ -26,4 +26,12 @@ public class Constants {
 
     public static final String SPRING_APPLICATION_CONTEXT = "springApplicationContext";
     public static final String BEAN_NAME_SPRING_APPLICATION_CONTEXT_PROVIDER = "springApplicationContextProvider";
+
+    /** mongodb配置 */
+    public static final String DEFAULT_MONGO_URL = "210.22.22.150";
+    public static final Integer DEFAULT_MONGO_PORT = 2091;
+    public static final String DEFAULT_MONGO_USERNAME = "scalog";
+    public static final String DEFAULT_MONGO_PASSWORD = "scalog_NB_$713!";
+    public static final String DEFAULT_MONGO_DB = "scalog";
+    public static final String DEFAULT_MONGO_DOCUMENT = "mongoLogInfo";
 }
--- src/main/java/com/java4all/scalog/utils/BaseHelper.java
+++ src/main/java/com/java4all/scalog/utils/BaseHelper.java
@@ -3,8 +3,11 @@ package com.java4all.scalog.utils;
 import org.springframework.beans.BeanUtils;
 import org.springframework.util.CollectionUtils;
 
+import java.lang.reflect.Field;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Description:对象copy工具类
@@ -21,24 +24,7 @@ public class BaseHelper {
      * @return
      * @apiNote resource to target
      */
-    @Deprecated
-    public static <T, R> T r2t(R resource, T target) {
-        if (resource == null) {
-            return null;
-        }
-        BeanUtils.copyProperties(resource, target);
-        return target;
-    }
-
-    /**
-     * @param resource
-     * @param target
-     * @param <T>
-     * @param <R>
-     * @return
-     * @apiNote resource to target
-     */
-    public static <T, R> T r2t(R resource, Class<T> target) {
+    public static <T, R> T convertTotarget(R resource, Class<T> target) {
         if (resource == null) {
             return null;
         }
@@ -60,15 +46,42 @@ public class BaseHelper {
      * @return
      * @apiNote resourceList to target type list
      */
-    public static <T, R> List<T> resourceList2target(List<R> resourceList, Class<T> target) {
+    public static <T, R> List<T> convertTotargetList(List<R> resourceList, Class<T> target) {
         List<T> tList = new ArrayList<>();
         if (CollectionUtils.isEmpty(resourceList)) {
             return tList;
         }
         resourceList.forEach(resource -> {
-            T t1 = BaseHelper.r2t(resource, target);
+            T t1 = BaseHelper.convertTotarget(resource, target);
             tList.add(t1);
         });
         return tList;
+    }
+
+    /**
+     * 对象转map
+     * @param obj
+     * @return
+     */
+    public static Map<String, Object> objectToMap(Object obj) {
+        Map<String, Object> map = new HashMap<String, Object>();
+        Field[] fields = obj.getClass().getDeclaredFields();	// 获取f对象对应类中的所有属性域
+        for (int i = 0, len = fields.length; i < len; i++) {
+            String varName = fields[i].getName();
+            try {
+                boolean accessFlag = fields[i].isAccessible();	// 获取原来的访问控制权限
+                fields[i].setAccessible(true);					// 修改访问控制权限
+                Object o = fields[i].get(obj);					// 获取在对象f中属性fields[i]对应的对象中的变量
+                if (o != null){
+                    map.put(varName, o.toString());
+                }
+                fields[i].setAccessible(accessFlag);			// 恢复访问控制权限
+            } catch (IllegalArgumentException ex) {
+                ex.printStackTrace();
+            } catch (IllegalAccessException ex) {
+                ex.printStackTrace();
+            }
+        }
+        return map;
     }
 }
