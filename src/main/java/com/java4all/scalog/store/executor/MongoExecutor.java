4a13d2a89ecb242f1ac2ba2715bebd0f556b152a
党诗林 <dangshilin@touchealth.com>
2020-08-10 11:07:27 +0800

    获取mongodb连接
    mongodb 实现

---
+++ src/main/java/com/java4all/scalog/store/executor/MongoExecutor.java
@@ -0,0 +1,45 @@
+package com.java4all.scalog.store.executor;
+
+import com.alibaba.fastjson.JSON;
+import com.java4all.scalog.store.LogInfoDto;
+import com.java4all.scalog.store.MongoLogInfo;
+import com.java4all.scalog.utils.BaseHelper;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Map;
+
+import static com.java4all.scalog.configuration.Constants.DEFAULT_MONGO_DB;
+
+/**
+ * MongoDB Executor
+ */
+public class MongoExecutor implements BaseSqlExecutor{
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(MongoExecutor.class);
+
+    protected MongoDatabase dataSource = null;
+
+    public MongoExecutor() {
+    }
+
+    public MongoExecutor(MongoDatabase dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Override
+    public void insert(LogInfoDto dto) throws Exception {
+        //获取集合
+        MongoCollection<Document> collection = dataSource.getCollection(DEFAULT_MONGO_DB);
+
+        //要插入的数据
+        MongoLogInfo logInfo = BaseHelper.r2t(dto, MongoLogInfo.class);
+        Map map = JSON.parseObject(JSON.toJSONString(logInfo),Map.class);
+        Document document = new Document(map);
+        //插入一个文档
+        collection.insertOne(document);
+    }
+}
--- src/main/java/com/java4all/scalog/store/source/MongoDataSourceGenerator.java
+++ src/main/java/com/java4all/scalog/store/source/MongoDataSourceGenerator.java
@@ -1,5 +1,15 @@
 package com.java4all.scalog.store.source;
 
+import com.mongodb.MongoClient;
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
+import com.mongodb.client.MongoDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.java4all.scalog.configuration.Constants.*;
+
 /**
  * @author wangzhongxiang
  */
@@ -7,6 +17,22 @@ public class MongoDataSourceGenerator implements SourceGenerator{
 
     @Override
     public Object generateSource() {
-        return null;
+        List<ServerAddress> adds = new ArrayList<>();
+        // ServerAddress()两个参数分别为 服务器地址 和 端口
+        ServerAddress serverAddress = new ServerAddress(DEFAULT_MONGO_URL, DEFAULT_MONGO_PORT);
+        adds.add(serverAddress);
+
+        List<MongoCredential> credentials = new ArrayList<>();
+        // MongoCredential.createScramSha1Credential()三个参数分别为 用户名 数据库名称 密码
+        MongoCredential mongoCredential = MongoCredential.createScramSha1Credential(DEFAULT_MONGO_USERNAME, DEFAULT_MONGO_DB, DEFAULT_MONGO_PASSWORD.toCharArray());
+        credentials.add(mongoCredential);
+
+        //通过连接认证获取MongoDB连接
+        MongoClient mongoClient = new MongoClient(adds, credentials);
+
+        //连接到数据库
+        MongoDatabase mongoDatabase = mongoClient.getDatabase(DEFAULT_MONGO_DB);
+        //返回连接数据库对象
+        return mongoDatabase;
     }
 }
